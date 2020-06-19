0cd6865a931ce84c4a24401cc4a79e41829af624
gilbertekalea <gilbertekalea@users.noreply.github.com>
2020-06-19 19:09:03 -0400

    Changed

---
+++ src/Components/Card.js
@@ -0,0 +1,30 @@
+import React from "react";
+//import { robots } from "../robots";
+
+const Card = ({name,id,email, uni,grad})=> {
+    return  (
+        <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+            
+            <img src= {`https://robohash.org/${id}?size=200x200`} alt="robots"/>
+            <div>
+                <h2 className="tc">{name}</h2>
+                <p className="tc">{email}</p>
+                <p className="tc dark-blue">{uni}</p>
+            </div>
+        </div>
+    );
+}
+export default Card;
+
+//destructuring
+// const Card = ({name, email, id})=> {
+//     return  (
+//         <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+//             <img src={`https://robohash.org/${id}?size=200x200`} alt="robots"/>
+//             <div>
+//                 <h2 className="tc">{name}</h2>
+//                 <p className="tc">{email}</p>
+//             </div>
+//         </div>
+//     );
+// }
